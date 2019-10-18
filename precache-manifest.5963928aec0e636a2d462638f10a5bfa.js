7b4cae4f1064dd45515ddce4f94907f994e596f8
rmclaren77 <rmclaren@vt.edu>
2019-10-18 13:18:20 -0400

    Updates

---
+++ precache-manifest.5963928aec0e636a2d462638f10a5bfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8cf60e82dd2a079044b4927ab849542",
+    "url": "www.reillymclaren.com/index.html"
+  },
+  {
+    "revision": "423879eb22997e4eb17c",
+    "url": "www.reillymclaren.com/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "143b3382adf19eaf30a5",
+    "url": "www.reillymclaren.com/static/js/2.c3081de7.chunk.js"
+  },
+  {
+    "revision": "423879eb22997e4eb17c",
+    "url": "www.reillymclaren.com/static/js/main.00d2de70.chunk.js"
+  },
+  {
+    "revision": "fcbabecae8b9e0599812",
+    "url": "www.reillymclaren.com/static/js/runtime-main.1d17cc78.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "www.reillymclaren.com/static/media/logo.25bf045c.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "www.reillymclaren.com/precache-manifest.5963928aec0e636a2d462638f10a5bfa.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("www.reillymclaren.com/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
